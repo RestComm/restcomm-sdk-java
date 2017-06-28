a76d7b6db84ba9f6a002fb60e43d9c8fa69a5163
mithileshkarnati <mithileshkarnati@gmail.com>
2017-06-28 13:30:42 +0530

    Final patch for SMS API,this closes #7

--- restcomm-connect.java.sdk/src/main/java/org/restcomm/connect/java/sdk/SMS/FilteredSMSList.java
+++ restcomm-connect.java.sdk/src/main/java/org/restcomm/connect/java/sdk/SMS/FilteredSMSList.java
@@ -18,7 +18,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.restcomm.connect.java.sdk.Notifications;
+package org.restcomm.connect.java.sdk.SMS;
 
 import java.io.IOException;
 import org.restcomm.connect.java.sdk.http.*;
@@ -40,77 +40,77 @@ import org.apache.http.ParseException;
 import com.google.gson.annotations.SerializedName;
 import java.net.MalformedURLException;
 
-public class FilteredNotificationList extends NotificationList {
+public class FilteredSMSList extends SMSList {
 	
 	private static String BASE_URL;
 	private static Request request;
 	
-	public FilteredNotificationList(final String BASE_URL)
+	public FilteredSMSList(final String BASE_URL)
 	{
 		this.BASE_URL = BASE_URL;
 		request = new Request(HttpMethod.GET,BASE_URL);
 	}
-	public FilteredNotificationList Page(String value) throws MalformedURLException
+	public FilteredSMSList Page(String value) throws MalformedURLException
 	{
 		request.addGetParameters("Page", value);
 		return this;
 	}
-	public FilteredNotificationList NumPages(String value) 
+	public FilteredSMSList NumPages(String value) 
 	{
 		request.addGetParameters("NumPages", value);
 		return this;
 	}
-	public FilteredNotificationList PageSize(String value) 
+	public FilteredSMSList PageSize(String value) 
 	{
 		request.addGetParameters("PageSize", value);
 		return this;
 	}
-	public FilteredNotificationList Total(String value) 
+	public FilteredSMSList Total(String value) 
 	{
 		request.addGetParameters("Total", value);
 		return this;
 	}
-	public FilteredNotificationList Start(String value) 
+	public FilteredSMSList Start(String value) 
 	{
 		request.addGetParameters("Start", value);
 		return this;
 	}
-	public FilteredNotificationList End(String value) 
+	public FilteredSMSList End(String value) 
 	{
 		request.addGetParameters("End", value);
 		return this;
 	}
-	public FilteredNotificationList MessageText(String value) 
+	public FilteredSMSList To(String value) 
 	{
-		request.addGetParameters("MessageText", value);
+		request.addGetParameters("To", value);
 		return this;
 	}
-	public FilteredNotificationList RequestUrl(String value) 
+	public FilteredSMSList From(String value) 
 	{
-		request.addGetParameters("RequestUrl", value);
+		request.addGetParameters("From", value);
 		return this;
 	}
-	public FilteredNotificationList ErrorCode(String value) 
+	public FilteredSMSList Body(String value) 
 	{
-		request.addGetParameters("ErrorCode", value);
+		request.addGetParameters("Body", value);
 		return this;
 	}
-	public FilteredNotificationList EndTime(String value) 
+	public FilteredSMSList EndTime(String value) 
 	{
 		request.addGetParameters("EndTime", value);
 		return this;
 	}
-	public FilteredNotificationList StartTime(String value) 
+	public FilteredSMSList StartTime(String value) 
 	{
 		request.addGetParameters("StartTime", value);
 		return this;
 	}
 	
-	public NotificationList Filter() 
+	public SMSList Filter() 
 	{
 		Restcomm.sendRequest(request);
 		Gson gson = new Gson();
-		return gson.fromJson(Restcomm.getJSONResponse(),NotificationList.class);
+		return gson.fromJson(Restcomm.getJSONResponse(),SMSList.class);
 	}
 
     
--- restcomm-connect.java.sdk/src/main/java/org/restcomm/connect/java/sdk/Utilities.java
+++ restcomm-connect.java.sdk/src/main/java/org/restcomm/connect/java/sdk/Utilities.java
@@ -28,6 +28,7 @@ import org.restcomm.connect.java.sdk.Applications.Application;
 
 import org.restcomm.connect.java.sdk.Accounts.Account;
 import org.restcomm.connect.java.sdk.Clients.Client;
+import org.restcomm.connect.java.sdk.SMS.SMS;
 import org.restcomm.connect.java.sdk.Notifications.Notification;
 
 public class Utilities {
@@ -56,5 +57,10 @@ public class Utilities {
 		
 		Gson gson = new Gson();
 		return gson.fromJson(jsonResponse,Notification.class);
+	}
+	public static SMS SMSObject(String jsonResponse) {
+		
+		Gson gson = new Gson();
+		return gson.fromJson(jsonResponse,SMS.class);
 	}
 }
